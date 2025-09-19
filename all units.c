aa1be7c3f9a15e1291d207f6516c209042365c72
vino29386 <vincentmelvinkimanthi@gmail.com>
2025-09-19 12:46:33 +0300

    Add files via upload

---
+++ all units.c
@@ -0,0 +1,25 @@
+//simple all units for the semester programme
+/*
+Name:vincent_melvin_kimanthi
+Reg no :PA106/G/29386/25
+Description:program to display all unit
+*/
+#include<stdio.h>//preprpcessors directive:scanf(),printf()
+
+//main function
+int main() {
+   printf("Discrete Mathematics SPM2100!!\n");
+   printf("Mathematics for Science SPM2104!!\n");
+   printf("Fundamentals of ICT UCU2105!!\n");
+   printf("Computer Systems and Organisation SPI2102!!\n");
+   printf("Communication Skills UCU2101!!\n");
+   printf("Introduction to Programming & Problem Solving SPC2101!!\n");
+   printf("Fundamentals of Systems Analysis & Design SSE2101!!\n");
+   
+   return 0;
+   }
+   
+  
+   
+
+    
